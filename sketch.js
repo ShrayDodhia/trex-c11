96d3e17f2d7899e262a4153929b229a8196c4c5d
ShrayDodhia <119731619+ShrayDodhia@users.noreply.github.com>
2022-12-10 11:50:21 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,72 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
-function preload() {
-    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-    trex_collided = loadImage("trex_collided.png");
-    groundImage = loadImage("ground2.png")
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
-function setup() {
-    createCanvas(600, 200);
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
 
-    //create a trex sprite
-    trex = createSprite(50,160,20,50);
-    trex.addAnimation("running", trex_running);
-    trex.scale = 0.5;
+//creating boy running
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
+  
 
-    //create a ground sprite
-    ground = createSprite(200,180,400,20);
-    ground.addImage("ground",groundImage);
-    ground.x = ground.width /2;
-    ground.velocityX = -4;
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible=false
 
-    invisibleGround = createSprite(200,190,400,10);
-    invisibleGround.visible=false
+// leftBoundary.invisible = false;
+// leftBoundary.visible = true;
+// leftBoundary.invisible = true;
+// leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
 }
 
 function draw() {
-    background(220);
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
 
-    //jump when the space button is pressed
-    if (keyDown("space")&&trex.y>=100) {
-        trex.velocityY = -10;
-    }
+  if (path.y > 300) {
+    path.y = 200;
+}
 
-    trex.velocityY = trex.velocityY + 0.8
+  
+  //code to reset the background
 
-    if (ground.x < 0) {
-        ground.x = ground.width / 2;
-    }
+  /*if(path.y > 400 ){
+    path.y = height/2;
+  }*/
 
-    trex.collide(invisibleGround);
-    drawSprites();
+  /*if(path.y > 400 ){
+   
+  path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
+path.y = height/2;}*/
+
+/*if(path.y > 400 ){path.y = height/2;}*/
+  
+  drawSprites();
 }
